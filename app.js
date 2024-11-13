d7aac3396b34f0e1cff08d86022a12901fe5ef70
GozzalSadirbaeva <sadirbaevagozzal@gmail.com>
2024-11-14 01:41:55 +0500

    done

--- app.js
+++ app.js
@@ -1,47 +1,32 @@
-let cardDatas = [
-  {
-    image:
-      "https://images.pexels.com/photos/13618926/pexels-photo-13618926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "Card 1",
-    description: "This is a card",
-    more: "See more",
-  },
-  {
-    image:
-      "https://images.pexels.com/photos/13618926/pexels-photo-13618926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "Card 2",
-    description: "This is a card",
-    more: "See more",
-  },
-];
+let cardDatas = JSON.parse(localStorage.getItem("cardDatas")) || [];
 
 let container = document.querySelector(".container");
 function createCards() {
-  cardDatas.forEach((val) => {
-    let cardDiv = document.createElement("div");
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let cardContent = document.createElement("div");
-    cardContent.classList.add("card-content");
-    let img = document.createElement("img");
-    let title = document.createElement("h4");
-    let description = document.createElement("p");
-    let more = document.createElement("button");
-    more.classList.add("more");
-    let removeBtn = document.createElement("button");
-    removeBtn.classList.add("fa-solid", "fa-trash", "absolute-block");
-    removeBtn.addEventListener("click", () => {
-      card.remove();
-    });
+  cardDatas &&
+    cardDatas.forEach((val) => {
+      let card = document.createElement("div");
+      card.classList.add("card");
+      let cardContent = document.createElement("div");
+      cardContent.classList.add("card-content");
+      let img = document.createElement("img");
+      let title = document.createElement("h4");
+      let description = document.createElement("p");
+      let more = document.createElement("button");
+      more.classList.add("more");
+      let removeBtn = document.createElement("button");
+      removeBtn.classList.add("fa-solid", "fa-trash", "absolute-block");
+      removeBtn.addEventListener("click", () => {
+        card.remove();
+      });
 
-    img.src = val.image;
-    title.textContent = val.title;
-    description.textContent = val.description;
-    more.textContent = val.more;
-    cardContent.append(title, description, more);
-    card.append(img, removeBtn, cardContent);
-    container.append(card);
-  });
+      img.src = val.image;
+      title.textContent = val.title;
+      description.textContent = val.description;
+      more.textContent = val.more;
+      cardContent.append(title, description, more);
+      card.append(img, removeBtn, cardContent);
+      container.append(card);
+    });
   cardDatas = [];
 }
 createCards();
@@ -66,7 +51,13 @@ addBtn.addEventListener("click", (e) => {
     description: addDescription.value,
     more: "See more",
   };
+
   cardDatas.push(data);
+
+  const oldCards = JSON.parse(localStorage.getItem("cardDatas"));
+  oldCards.push(data);
+
+  localStorage.setItem("cardDatas", JSON.stringify(oldCards));
   createCards();
   addNewCard.reset();
 });
